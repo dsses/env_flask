49a9f8a7391d68efd217eec373f0bd03bd166505
dsses <dsses00@gmail.com>
2021-01-17 22:29:12 +0900

    1st commit

---
+++ web/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  conn = mysql.connector.connect(user='my', password='my', host='flask_db', database='my')
+  cur = conn.cursor()
+  cur.execute("select * from book")
+  books = cur.fetchall()
+  cur.close()
+  conn.close()
+
+  html = render_template('index.html', books=books)
+  return html
+
+
+if __name__ == '__main__':
+  app.run(port=5000)
